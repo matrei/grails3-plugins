922a03edcb82a3510c788882dfd4c9af151bad89
sheehan <mr.sheehan@gmail.com>
2016-10-05 10:24:09 -0500

    dont refresh in bootstrap

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,18 +1,13 @@
-import com.github.sheehan.PluginService
 import com.github.sheehan.Plugins
 import grails.util.Environment
 import groovy.json.JsonSlurper
 
 class BootStrap {
 
-    PluginService pluginService
-
     def init = { servletContext ->
         if (Environment.developmentMode) {
             List plugins = new JsonSlurper().parseText(this.class.getClassLoader().getResourceAsStream('plugins.json').text) as List
             Plugins.set plugins
-        } else {
-            pluginService.refreshPlugins()
         }
     }
 
