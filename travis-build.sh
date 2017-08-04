068198c85130cbdebf4d07252aa20e53fbba4b32
sdelamo <sergio.delamo@softamo.com>
2017-08-04 08:42:50 +0200

    generate war file with gradle wrapper

--- travis-build.sh
+++ travis-build.sh
@@ -11,17 +11,17 @@ if [[ $EXIT_STATUS ]]; then
 
     if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
-        if [[ -n $TRAVIS_TAG ]]; then
+        # if [[ -n $TRAVIS_TAG ]]; then
 
             echo "Publishing to PWS"
 
-            ./grailsw war || EXIT_STATUS=$?
+            ./gradlew clean war || EXIT_STATUS=$?
 
             if [[ $EXIT_STATUS ]]; then
 
                 ./gradlew -PcfUsername=$CF_USERNAME -PcfPassword=$CF_PASSWORD cfPush || EXIT_STATUS=$i
             fi
-        fi
+        # fi
     fi
 fi
 exit $EXIT_STATUS
