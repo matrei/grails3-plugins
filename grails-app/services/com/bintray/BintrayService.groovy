b1dd15b0a5adfce9291d8ecf11242901b28cb4aa
Sergio del Amo <sergio.delamo@softamo.com>
2019-12-20 12:15:31 +0100

    replace OKHttp/moshi with Micronaut HTTP Client

--- grails-app/services/com/bintray/BintrayService.groovy
+++ grails-app/services/com/bintray/BintrayService.groovy
@@ -1,98 +1,76 @@
 package com.bintray
 
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
 import grails.config.Config
 import grails.core.support.GrailsConfigurationAware
+import groovy.transform.CompileDynamic
+import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
-import okhttp3.Headers
-import okhttp3.HttpUrl
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
-import java.lang.reflect.Type
+import io.micronaut.core.type.Argument
+import io.micronaut.core.type.Headers
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.client.BlockingHttpClient
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.exceptions.HttpClientResponseException
+import io.micronaut.http.uri.UriBuilder
 
+@CompileStatic
 @Slf4j
 class BintrayService implements GrailsConfigurationAware {
-
-    private final Moshi moshi = new Moshi.Builder().build()
-    private final OkHttpClient client = new OkHttpClient()
-    private final JsonAdapter<BintrayPackage> bintrayPackageJsonAdapter = moshi.adapter(BintrayPackage.class)
-
 
     private static final String BINTRAY_API_URL = "https://api.bintray.com";
 
     String organization
     String repository
-    String username
     String token
+    String username
+    BlockingHttpClient client = HttpClient.create(new URL(BINTRAY_API_URL)).toBlocking()
 
     @Override
     void setConfiguration(Config co) {
         organization = co.getProperty('bintray.organization', String, 'grails')
         repository = co.getProperty('bintray.repository', String, 'plugins')
-        username = co.getProperty('bintray.username', String)
-        token = co.getProperty('bintray.token', String)
-    }
-
-    String authorization() {
-        String encoded = "$username:$token".bytes.encodeBase64()
-        "Basic $encoded".toString()
+        this.username = co.getProperty('bintray.username', String)
+        this.token = co.getProperty('bintray.token', String)
     }
 
     BintrayPackage fetchBintrayPackage(String name, String organization = this.organization, String repository = this.repository) throws IOException {
-        final String url = "${BINTRAY_API_URL}/packages/${organization}/${repository}/${name}".toString()
-        Request request = new Request.Builder()
-                .url(url)
-                .header("Authorization", authorization())
-                .build()
-        Response response = client.newCall(request).execute()
-        BintrayPackage bintrayPackage
-        if ( response.isSuccessful() ) {
-            bintrayPackage = bintrayPackageJsonAdapter.fromJson(response.body().source())
-        } else {
-            log.warn 'Response {}. Could not fetch bintray package at {}', response.code(), name
+        final String url = "/packages/${organization}/${repository}/${name}".toString()
+        HttpResponse<BintrayPackage> response = null
+        try {
+            log.trace("sending request to {}", url)
+            response = client
+                    .exchange(HttpRequest.GET(url).basicAuth(username, token), BintrayPackage)
+            log.trace("fetched bintray packages {}", url)
+            return response?.body()
+
+        } catch(HttpClientResponseException e) {
+            log.warn 'Response {}. Could not fetch bintray package at {}', response?.status?.code, name
         }
-        response.close()
-        return bintrayPackage
+        null
     }
 
+    @CompileDynamic
     BintrayPackageResponse fetchBintrayPackagesByStartPosition(Integer startPos) throws NumberFormatException, IOException {
-        log.info 'Fetching bintray packaging at position: {}', startPos
-        final String url = "${BINTRAY_API_URL}/repos/grails/plugins/packages"
-        final Map<String, String> params = new HashMap<>()
-        params.put("start_pos", String.valueOf(startPos))
-        HttpUrl.Builder httpBuider = HttpUrl.parse(url).newBuilder()
-        if (params != null) {
-            for(Map.Entry<String, String> param : params.entrySet()) {
-                httpBuider.addQueryParameter(param.getKey(),param.getValue())
-            }
-        }
+        log.trace 'Fetching bintray packaging at position: {}', startPos
+        String url = UriBuilder.of("/repos/grails/plugins/packages").queryParam("start_pos", startPos).build()
+        log.debug("fetching {}", url)
 
-        Request request = new Request.Builder()
-                .url(httpBuider.build())
-                .header("Authorization", authorization())
-                .build()
-        Response response = client.newCall(request).execute()
-        BintrayPackageResponse rsp
-        if ( response.isSuccessful() ) {
+        HttpResponse<List<BintrayPackageSimple>> response = null
+        try {
+            response = client.exchange(HttpRequest.GET(url).basicAuth(username, token), Argument.of(List, BintrayPackageSimple))
 
-            String bintrayPackageJsonResponse = response.body().string()
-            Type type = Types.newParameterizedType(List.class, BintrayPackageSimple.class)
-            JsonAdapter<List<BintrayPackageSimple>> adapter = moshi.adapter(type)
-            List<BintrayPackageSimple> bintrayPackageList = adapter.fromJson(bintrayPackageJsonResponse)
+            log.trace("fetched {}", url)
+            List<BintrayPackageSimple> bintrayPackageList = response?.body()
 
-            Headers headers = response.headers()
-            rsp = new BintrayPackageResponse(start: startPosition(headers),
-                    end: endPositionHeader(headers),
-                    total: totalHeader(headers),
+            return new BintrayPackageResponse(start: startPosition(response.headers),
+                    end: endPositionHeader(response.headers),
+                    total: totalHeader(response.headers),
                     bintrayPackageList: bintrayPackageList)
-        } else {
-            log.warn 'Response {}. Could not fectch bintray packages at {}', response.code(), startPos
+        } catch(HttpClientResponseException e) {
+            log.warn 'Response {}. Could not fectch bintray packages at {}', response.status.code, startPos
+
         }
-        response.close()
-        rsp
     }
 
     List<Integer> expectedExtraStarPositions(Integer total, Integer startPosition, Integer endPosition) {
@@ -114,29 +92,17 @@ class BintrayService implements GrailsConfigurationAware {
     }
 
     private Integer totalHeader(Headers headers) {
-        Integer total = null
-        List<String> rangeLimitTotalValues = headers.values("X-RangeLimit-Total")
-        if (rangeLimitTotalValues != null && !rangeLimitTotalValues.isEmpty()) {
-            total = Integer.valueOf(rangeLimitTotalValues.get(0))
-        }
-        total
+        Optional<Integer> optionalInteger = headers.get("X-RangeLimit-Total", Integer)
+        optionalInteger.isPresent() ? optionalInteger.get() : null
     }
 
     private Integer startPosition(Headers headers) {
-        Integer startPosition = null
-        List<String> rangeLimitStartPositionValues = headers.values("X-RangeLimit-StartPos")
-        if (rangeLimitStartPositionValues != null && !rangeLimitStartPositionValues.isEmpty()) {
-            startPosition = Integer.valueOf(rangeLimitStartPositionValues.get(0))
-        }
-        startPosition
+        Optional<Integer> optionalInteger = headers.get("X-RangeLimit-StartPos", Integer)
+        optionalInteger.isPresent() ? optionalInteger.get() : null
     }
 
     private Integer endPositionHeader(Headers headers) {
-        Integer endPosition = null
-        List<String> rangeLimitEndPositionValues = headers.values("X-RangeLimit-EndPos")
-        if (rangeLimitEndPositionValues != null && !rangeLimitEndPositionValues.isEmpty()) {
-            endPosition = Integer.valueOf(rangeLimitEndPositionValues.get(0))
-        }
-        endPosition
+        Optional<Integer> optionalInteger = headers.get("X-RangeLimit-EndPos", Integer)
+        optionalInteger.isPresent() ? optionalInteger.get() : null
     }
 }
--- grails-app/services/com/github/GithubService.groovy
+++ grails-app/services/com/github/GithubService.groovy
@@ -1,29 +1,25 @@
 package com.github
 
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
 import grails.config.Config
 import grails.core.support.GrailsConfigurationAware
 import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
-import okhttp3.HttpUrl
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.client.BlockingHttpClient
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 
 @CompileStatic
 @Slf4j
 class GithubService implements GrailsConfigurationAware {
 
-    private final Moshi moshi = new Moshi.Builder().build()
-    private final OkHttpClient client = new OkHttpClient()
-    private final JsonAdapter<GithubRepository> githubRepositoryJsonAdapter = moshi.adapter(GithubRepository.class)
-
     private static final String GITHUB_API_URL = 'https://api.github.com'
     String username
     String token
     String userAgent
+    BlockingHttpClient client = HttpClient.create(new URL(GITHUB_API_URL)).toBlocking()
 
     @Override
     void setConfiguration(Config config) {
@@ -31,12 +27,6 @@ class GithubService implements GrailsConfigurationAware {
         token = config.getProperty('github.token', String)
         userAgent = config.getProperty('github.userAgent', String, 'Grails Plugins')
     }
-
-    String authorization() {
-        String encoded = "$username:$token".bytes.encodeBase64()
-        "Basic $encoded".toString()
-    }
-
 
     @CompileDynamic
     String ownerAndRepo(String vcsUrl) {
@@ -52,21 +42,17 @@ class GithubService implements GrailsConfigurationAware {
         if ( !ownerAndRepo ) {
             return null
         }
-        final String url = "${GITHUB_API_URL}/repos/${ownerAndRepo}".toString()
-        HttpUrl.Builder httpBuider = HttpUrl.parse(url).newBuilder()
-        Request request = new Request.Builder()
-                .url(httpBuider.build())
-                .header("Authorization", authorization())
-                .header("User-Agent", userAgent)
-                .build()
-        Response response = client.newCall(request).execute()
-        GithubRepository githubRepository
-        if ( response.isSuccessful() ) {
-            githubRepository = githubRepositoryJsonAdapter.fromJson(response.body().source())
-        } else {
-            log.warn 'Response {}. Could not fetch github repository at {}', response.code(), vcsUrl
+        final String url = "/repos/${ownerAndRepo}".toString()
+        HttpResponse<GithubRepository> response = null
+        try {
+            response = client.exchange(HttpRequest.GET(url).basicAuth(username, token)
+                    .header("User-Agent", userAgent), GithubRepository)
+            log.trace("fetched {} github repository", ownerAndRepo)
+            return response.body()
+        } catch(HttpClientResponseException e) {
+            log.warn 'Response {}. Could not fetch github repository at {}', response?.status?.code, vcsUrl
+
         }
-        response.close()
-        githubRepository
+        null
     }
 }
